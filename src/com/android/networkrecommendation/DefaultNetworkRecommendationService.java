f40ebd559d99b0204134f5433000a32c9e8ad005
Joe LaPenna <jlapenna@google.com>
2017-01-03 18:33:05 -0800

    Formatting and documentation updates.
    Test: adb shell am instrument -w com.android.networkrecommendation.tests/android.support.test.runner.AndroidJUnitRunner
    Bug: 33224286
    Change-Id: I73b230d56ebeccd15e106ab8c931348074158179

--- src/com/android/networkrecommendation/DefaultNetworkRecommendationService.java
+++ src/com/android/networkrecommendation/DefaultNetworkRecommendationService.java
@@ -29,42 +29,6 @@ import java.io.PrintWriter;
 
 /**
  * Provides network recommendations for the platform.
- *
- * <p>This example evaluates networks in a scan and picks the "least bad" network, returning a
- * result to the RecommendedNetworkEvaluator, regardless of configuration point.
- *
- * <p>This recommender is not yet recommended for non-development devices.
- *
- * <p>To debug:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService
- *
- * <p>Clear stored scores:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService clear
- *
- * <p>Score a network:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService addScore $SCORE
- *
- * <p>SCORE: "Quoted SSID",bssid|$RSSI_CURVE|metered|captivePortal
- *
- * <p>RSSI_CURVE: start,bucketWidth,score,score,score,score,...
- *
- * <p>curve, metered and captive portal are optional, as expressed by an empty value.
- *
- * <p>All commands should be executed on one line, no spaces between each line of the command..
- * <p>Eg, A high quality, paid network with captive portal:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService addScore \
- * '\"Metered\",aa:bb:cc:dd:ee:ff\|
- * -150,10,-128,-128,-128,-128,-128,-128,-128,-128,27,27,27,27,27,-128\|1\|1'
- *
- * <p>Eg, A high quality, unmetered network with captive portal:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService addScore \
- * '\"Captive\",aa:bb:cc:dd:ee:ff\|
- * -150,10,-128,-128,-128,-128,-128,-128,-128,-128,28,28,28,28,28,-128\|0\|1'
- *
- * <p>Eg, A high quality, unmetered network with any bssid:
- * $ adb shell dumpsys activity service DefaultNetworkRecommendationService addScore \
- * '\"AnySsid\",00:00:00:00:00:00\|
- * -150,10,-128,-128,-128,-128,-128,-128,-128,-128,29,29,29,29,29,-128\|0\|0'
  */
 public class DefaultNetworkRecommendationService extends Service {
 
